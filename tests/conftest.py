5ba1bbbf63e8a3228e33c8237b69e6f5ea831930
Andreas Maier <andreas.r.maier@gmx.de>
2020-03-07 12:05:23 +0100

    Fixed Pylint issues and activated pylint rc checking
    Signed-off-by: Andreas Maier <andreas.r.maier@gmx.de>

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,7 @@
+"""
+Module automatically picked up by pytest.
+"""
+# pylint: disable=invalid-name
 
 # Add the 'pytester' plugin that is used for testing pytest plugins.
 pytest_plugins = 'pytester'
